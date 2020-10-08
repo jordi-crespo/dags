a2190346ae25e9a0e435fe3a45bb2765184dc968
jordi-crespo <jordi.kg@gmail.com>
2020-10-08 14:40:16 +0200

    Update dag_test_jordi1.py

--- dag_test_jordi1.py
+++ dag_test_jordi1.py
@@ -3,39 +3,52 @@ from datetime import datetime, timedelta
 from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
 from airflow.operators.dummy_operator import DummyOperator
 from kubernetes.client import models as k8s
+import logging
+import os
+import sys
+import traceback 
+
+try:
+
+    default_args = {
+        'owner': 'airflow',
+        'depends_on_past': False,
+        'start_date': datetime.utcnow(),
+        'email': ['airflow@example.com'],
+        'email_on_failure': False,
+        'email_on_retry': False,
+        'retries': 1,
+        'retry_delay': timedelta(minutes=5)
+    }
+
+    dag = DAG(
+        'jordi_test1', default_args=default_args, schedule_interval=timedelta(minutes=10))
 
 
+    start = DummyOperator(task_id='run_this_first', dag=dag)
 
-default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'start_date': datetime.utcnow(),
-    'email': ['airflow@example.com'],
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5)
-}
+    quay_k8s = KubernetesPodOperator(
+            namespace='default',
+            image='marcelotest',
+            image_pull_secrets=[k8s.V1LocalObjectReference('azure-registry')],
+            cmds=["python", "main.py"],
+            arguments=["echo", "10", "echo pwd"],
+            labels={"foo": "bar"},
+            name="marcelotest",
+            is_delete_operator_pod=True,
+            in_cluster=True,
+            task_id="task-two",
+            get_logs=True,
+            dag=dag
+        )
 
-dag = DAG(
-    'jordi_test1', default_args=default_args, schedule_interval=timedelta(minutes=10))
+    start >> quay_k8s
+except Exception as e:
 
-
-start = DummyOperator(task_id='run_this_first', dag=dag)
-
-quay_k8s = KubernetesPodOperator(
-        namespace='default',
-        image='marcelotest',
-        image_pull_secrets=[k8s.V1LocalObjectReference('azure-registry')],
-        cmds=["python", "main.py"],
-        arguments=["echo", "10", "echo pwd"],
-        labels={"foo": "bar"},
-        name="marcelotest",
-        is_delete_operator_pod=True,
-        in_cluster=True,
-        task_id="task-two",
-        get_logs=True,
-        dag=dag
-    )
-
-start >> quay_k8s
+    error_message = {
+            "message": "An internal error ocurred"
+            ,"error": str(e)
+            , "error information" : str(sys.exc_info())
+            , "traceback": str(traceback.format_exc())
+        }
+    logging(error_message)
